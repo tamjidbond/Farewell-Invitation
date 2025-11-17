fcfe2c961701fcceb54b7cc9d5cbd639889c6432
Tamjid Bond <hello@Tamjids-Macbook.local>
2025-11-17 22:12:50 +0600

    to

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
 
             <Link
-                href="https://tamjidbond-portfolio.netlify.app/"
+                to="https://tamjidbond-portfolio.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-cyan-400 hover:underline"
