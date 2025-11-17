e5a4581b8271b045ec13ca7c5f8feb1994d7233c
Tamjid Bond <hello@Tamjids-Macbook.local>
2025-11-17 22:18:34 +0600

    fix

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <footer className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
+        <div className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
 
             <Link
-                to="https://tamjidbond-portfolio.netlify.app/"
+                href="https://tamjidbond-portfolio.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-cyan-400 hover:underline"
             >
                 &copy;  Tamjid Bond
             </Link>
-        </footer>
+        </div>
     );
 };
 
