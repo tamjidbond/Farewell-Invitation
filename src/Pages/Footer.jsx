0e1953ee05edef7e076abbfa7744f297069f42ba
Tamjid Bond <hello@Tamjids-Macbook.local>
2025-11-17 22:28:36 +0600

    remove external link

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ExternalLink from './ExternalLink';
 
 const Footer = () => {
     return (
-        <div className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
-            <ExternalLink
-                to="https://tamjidbond-portfolio.netlify.app/"
+        <footer className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
+
+            <a
+                href="https://tamjidbond-portfolio.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="text-cyan-400 hover:underline"
             >
-                &copy; Tamjid Bond
-            </ExternalLink>
-        </div>
+                &copy;MD. Tamjid Bond
+            </a>
+        </footer>
     );
 };
 
