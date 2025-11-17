d81851d827ad236003d14cfb24b580c6542e3f18
Tamjid Bond <hello@Tamjids-Macbook.local>
2025-11-17 22:12:16 +0600

    click

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
         <footer className="absolute bottom-6 sm:bottom-10 w-full text-center text-sm sm:text-base opacity-70">
 
-            <a
+            <Link
                 href="https://tamjidbond-portfolio.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-cyan-400 hover:underline"
             >
                 &copy;  Tamjid Bond
-            </a>
+            </Link>
         </footer>
     );
 };
